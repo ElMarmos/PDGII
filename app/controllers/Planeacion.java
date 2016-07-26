489e11a2761947ec9827270dbbc6df4c55dd827c
Jhonatan España <jersi2695@hotmail.com>
2016-07-26 00:00:42 -0500

    Vista de programaciones, y diagrama de la programacion

--- app/controllers/Planeacion.java
+++ app/controllers/Planeacion.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,11 +9,19 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.persistence.Query;
 
 import org.apache.log4j.helpers.QuietWriter;
 import org.h2.constant.SysProperties;
+import org.json.JSONArray;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 import models.Cirugia;
 import models.Indicador;
@@ -30,19 +39,55 @@ public class Planeacion extends Controller {
 		 render(programaciones);
 	 }
 	 
-	 public static void programacionesPorFecha (Date fechaProgramacion){
-		 Query query = JPA.em().createQuery("SELECT p, AVG(i.valor) as avgindicador FROM "
+	 public static void programacionesPorFecha (String dateProgramacion){
+		 DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S", Locale.ENGLISH);
+		 try {
+			Date date = format.parse(dateProgramacion);
+			
+			Query query = JPA.em().createQuery("SELECT p, AVG(i.valor) as avgindicador FROM "
 			 		+ "Programacion p LEFT JOIN p.programacionIndicadores i WHERE p.fechaProgramacion = :datepro GROUP BY p.idProgramacion ORDER BY avgindicador DESC");
-		 
-		 query.setParameter("datepro", fechaProgramacion);
-		 List<Object[]> programaciones = query.getResultList();
-		 render(programaciones);
+			query.setParameter("datepro", date);
+			List<Object[]> programaciones = query.getResultList();
+			render(programaciones);
+		 } catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 	 }
 	 
 	 public static void programacionDetails(String idProgramacion){
+		 
 		 Programacion programacion = Programacion.findById(Integer.parseInt(idProgramacion));
 		 List<Cirugia> cirugias = Cirugia.find("byProgramacion", programacion ).fetch();
 		 render(cirugias);
+		 
+		 /**try{
+			 Programacion programacion = Programacion.findById(Integer.parseInt(idProgramacion));
+			 
+			 System.out.println(dcirugias.size());
+			 System.out.println(dcirugias.get(0).getIdCirugia());
+			 JSONObject cirugias = new JSONObject();
+			 JSONArray arrSurge = new JSONArray();
+			 for (Cirugia cirugia : dcirugias) {
+				JSONObject surgery = new JSONObject();
+				surgery.put("idQuirofano", cirugia.getQuirofano().getIdQuirofano());
+				surgery.put("nombreQuirofano", cirugia.getQuirofano().getNombreQuirofano());
+				surgery.put("fechaIngreso", cirugia.getFechaIngreso());
+				surgery.put("horaCierre", cirugia.getHoraCierre());
+				arrSurge.put(surgery);
+			 }
+			 cirugias.put("cirugias", arrSurge);
+			 System.out.println(cirugias.toString());
+			 render(cirugias);
+		 }catch(JSONException je){
+			 String error = je.getMessage();
+			 render(error);
+		 }catch(Exception e){
+			 String error = e.getMessage();
+			 render(error);
+		 }
+		 */
 	 }
 	 
 	 
